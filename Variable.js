7d21a33ea4c0a863cd356ee2b8fe3e954dec3371
Natasha Callula Liora <120435315+natashacallula@users.noreply.github.com>
2024-09-03 19:31:30 +0700

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,7 @@
+//Inisiasi variabel x dan y
+let x = "10" + 10 + 10;
+let y = 10 + 10 +"10";
+
+//Menampilkan hasil penjumlahan x dan y 
+console.log("Hasil dari x:", x);
+console.log("Hasil dari y", y);
