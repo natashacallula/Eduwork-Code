4ef91798e297949b7a98cb2e89ca7bbb9260c2c5
natashacallula <natashacallula@gmail.com>
2024-09-03 21:05:48 +0700

    Commit ulang

---
+++ DataTypes.js
@@ -0,0 +1,12 @@
+//Tugas Latihan
+let length = 16; // Number
+let lastname = "Johnson"; // String
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+}; // Object
+
+// Menggunakan typeof untuk mengetahui tipe data
+console.log(typeof length); // Output: number
+console.log(typeof lastname); // Output: string
+console.log(typeof x); // Output: object
---
+++ syntax.js
@@ -0,0 +1,12 @@
+// Inisiasi Variabel
+let var1 = 10; //Varibel pertama = 10
+let var2 = "saya belajar javascript"; //Variabel kedua = string
+let var3 = 30; //Variabel ketiga = angka random
+
+// Melakukan penjumlahan antara variabel 1 dan variabel 2
+
+let hasil = var1+var2;
+console.log(`Hasil penjumlahan variabel 1 dan variabel 2 adalah ${hasil}`);
+
+let hasilfix = var1+var3;
+console.log(`Terjadi kesalahan, hasil sebenarnya adalah ${hasilfix}`);
