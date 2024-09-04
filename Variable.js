ee0179b60e97b79fec13085c7fa8557aed4fd206
natashacallula <natashacallula@gmail.com>
2024-09-04 11:19:18 +0700

    Remove Variable.js

--- Variable.js
+++
@@ -1,7 +0,0 @@
-//Inisiasi variabel x dan y
-let x = "10" + 10 + 10;
-let y = 10 + 10 +"10";
-
-//Menampilkan hasil penjumlahan x dan y 
-console.log("Hasil dari x:", x);
-console.log("Hasil dari y", y);
