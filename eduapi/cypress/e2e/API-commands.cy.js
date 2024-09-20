3f4059044cd66ee133fb28d2f27e92713cfc6aeb
natashacallula <natashacallula@gmail.com>
2024-09-20 21:49:02 +0700

    Upload progress

--- eduapi/cypress/e2e/API-commands.cy.js
+++ eduapi/cypress/e2e/API-commands.cy.js
@@ -5,6 +5,13 @@ describe('Test with login via API', () => {
 
     it('Should access the main dashboard', () => {
         cy.url().should('include', '/#!main');
-        cy.get('.dashboard').should('be.visible');
+        cy.get('#dashboard').should('be.visible'); 
+    });
+
+    it('Should have session cookies set', () => {
+        // Memastikan cookies telah ter-set setelah login via API
+        cy.getCookie('sessionID').should('exist');
+        cy.getCookie('userId').should('exist');
+        cy.getCookie('userName').should('exist');
     });
 });
